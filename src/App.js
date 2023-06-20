a8dd2e9f11c9223ab3175e28c5c1aad7284ad020
Antonio <pancake.engine@gmail.com>
2023-06-19 20:44:29 -0500

    pushin everythang

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './App.css';
+import firebase, { initializeApp } from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDh8YnaSUyoNoiAXxkzpyLp96_gps0RQV0",
+  authDomain: "chat-app-from-tutorial.firebaseapp.com",
+  projectId: "chat-app-from-tutorial",
+  storageBucket: "chat-app-from-tutorial.appspot.com",
+  messagingSenderId: "536310110082",
+  appId: "1:536310110082:web:6e4c04c77a309b6cede19b",
+  measurementId: "G-R6Q2030SM4"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const [user] = useAuthState(auth);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+
+      <section>
+        {user ? < ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.SignOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, {idField: 'id'});
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const {text, uid } = props.message;
+
+  return <p>{text}</p>
+}
+
+export default App;
